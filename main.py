548ec7e8c3e99f3bae8219ab1aab598950352837
kitso20 <motlashupingkitso@gmail.com>
2026-03-09 23:10:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,3 +28,16 @@ for i in range(n):
 
     # for new line
     print()
+
+n = 10
+a = 0
+b = 1
+next = b  
+count = 1
+
+while count <= n:
+    print(next, end=" ")
+    count += 1
+    a, b = b, next
+    next = a + b
+print()
