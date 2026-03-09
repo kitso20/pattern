25de71154278492c79b4b7fc02bafe4122029e4b
kitso20 <motlashupingkitso@gmail.com>
2026-03-09 23:08:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from math import factorial
+
 def trangle(n=5):
     for i in range(n):
         print(' '*(n - i - 1),end='')
@@ -10,4 +12,19 @@ def trangle(n=5):
             print('*' + ' ' * (2 * i -1) + '*')
     
 trangle(5)
-``
+
+# input n
+n = 5
+for i in range(n):
+    for j in range(n-i+1):
+
+        # for left spacing
+        print(end=" ")
+
+    for j in range(i+1):
+
+        # nCr = n!/((n-r)!*r!)
+        print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
+
+    # for new line
+    print()
